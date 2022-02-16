440dd8e286b98704256f47ba1584fe6da15db2e7
Bui Phuc Duyet <phucduyet.bui@mastercard.com>
2022-02-16 14:13:58 +0800

    pct

--- src/app/features/home/pipes/price.pipe.ts
+++ src/app/features/home/pipes/price.pipe.ts
@@ -10,7 +10,7 @@ export class PricePipe implements PipeTransform {
   transform(
     id: number,
     priceData: Nullable<Price[]>,
-    type: 'own' | 'comp' | 'tax'
+    type: 'own' | 'comp' | 'tax' | 'save'
   ): Nullable<number | Price['taxes_and_fees']> {
     const priceObj = priceData?.find(x => x.id === id)
 
@@ -21,10 +21,20 @@ export class PricePipe implements PipeTransform {
         return priceObj.price
       case 'comp': {
         if (!priceObj.competitors) return null
-        return Math.max(...Object.values(priceObj.competitors) as number[])
+        const price = this.transform(id, priceData, 'own') as number
+        const comps = Object.values(priceObj.competitors) as number[]
+        const maxComp = Math.max(...comps)
+        return maxComp > price ? maxComp : null
       }
       case 'tax':
         return priceObj.taxes_and_fees
+      case 'save': {
+        const price = this.transform(id, priceData, 'own') as number
+        const comp = this.transform(id, priceData, 'comp') as number
+        return comp && comp
+          ? Math.round((comp - price) / comp * 100)
+          : null
+      }
     }
   }
 }
